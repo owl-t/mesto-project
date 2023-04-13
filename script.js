4fbdd5132a166918b68a9c308d256bfba4347788
Aleksandr Davydov <d4vydovoliver@yandex.ru>
2023-04-13 14:03:39 +0700

    second-try

--- script.js
+++
@@ -1,8 +0,0 @@
-const form = document.querySelector(".form");
-document.querySelector(".form__close-button").onclick = function () {
-  form.style.display = "none";
-};
-
-document.querySelector(".profile__edit-button").onclick = function () {
-  form.style.display = "flex";
-};
